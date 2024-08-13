08d00c681ebd59a55bb24a1f660c49edc2e6431a
shankar7042 <shankar.kumar.4171@gmail.com>
2024-08-13 12:16:17 +0530

    show cart is empty

--- src/components/CartProducts.tsx
+++ src/components/CartProducts.tsx
@@ -7,7 +7,7 @@ import { ProductWithQuantity } from "@/context/CartContext";
 import Image from "next/image";
 
 const CartProducts = () => {
-  const { allCartItemProducts } = useCartContext();
+  const { allCartItemProducts, totalQuantity } = useCartContext();
   const [cartProducts, setCartProducts] = useState<ProductWithQuantity[]>([]);
 
   useEffect(() => {
@@ -15,7 +15,10 @@ const CartProducts = () => {
       setCartProducts(products);
     });
   }, [allCartItemProducts]);
-  return (
+
+  return totalQuantity === 0 ? (
+    <div className="text-center font-semibold text-xl my-4">Cart is empty</div>
+  ) : (
     <div className="border px-4 py-4 rounded-md flex flex-col gap-6">
       {cartProducts.map((product) => (
         <div key={product.id} className="flex items-center gap-4">
