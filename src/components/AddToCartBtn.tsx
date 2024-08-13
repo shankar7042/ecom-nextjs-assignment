57e8c0ac847da32e73bf30991b9956ffefb3d051
shankar7042 <shankar.kumar.4171@gmail.com>
2024-08-13 21:48:50 +0530

    added animation when add to cart

--- src/components/AddToCartBtn.tsx
+++ src/components/AddToCartBtn.tsx
@@ -2,6 +2,7 @@
 
 import { useCartContext } from "@/hooks/useCartContext";
 import { IProduct } from "@/types/product";
+import { useState } from "react";
 
 interface AddToCartBtnProps {
   product: IProduct;
@@ -9,16 +10,32 @@ interface AddToCartBtnProps {
 
 const AddToCartBtn: React.FC<AddToCartBtnProps> = ({ product }) => {
   const { addToCart } = useCartContext();
+  const [show, setShow] = useState(false);
+
   const handleClick = () => {
     addToCart(product.id);
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 700);
   };
 
   return (
     <button
       onClick={handleClick}
-      className="w-full py-2 bg-yellow-400 font-semibold mt-3 rounded-md transition duration-300 hover:bg-yellow-300"
+      className="w-full py-2 bg-yellow-400 font-semibold mt-3 rounded-md transition duration-300 hover:bg-yellow-300 relative"
     >
-      Add to Cart
+      Add to Cart{" "}
+      <span
+        className={`ml-4 absolute text-green-600
+        ${show ? "opacity-1" : "opacity-0"}
+        ${show ? "-translate-y-full" : "translate-y-full"}
+        transition-all
+        duration-700
+        `}
+      >
+        +1
+      </span>
     </button>
   );
 };
